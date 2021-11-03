a31e7d31498df3958c710aab66e7aedda6b67cd5
tosha504 <tosha.rovniy@gmail.com>
2021-11-03 09:55:24 +0100

    news

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ jQuery(document).ready(function ($) {
     $burger = $(".header__burger"),
     $body = $("body"),
     $header = $(".header"),
-    $logo = $(".header__logo img"),
     $slider = $(".programs__slider");
 
   if (!$body.hasClass("home")) {
-    $header
-      .css({
-        background: "linear-gradient(96.8deg, #E18C44 18.05%, #FFCC81 91.37%)",
-        padding: "20px",
-      })
-      .removeClass("header__logo")
-      .addClass(".header__logo_m");
+    console.log("home");
+    $header.css({
+      background: "linear-gradient(96.8deg, #E18C44 18.05%, #FFCC81 91.37%)",
+      padding: "20px",
+    });
+  } else {
+    $header.addClass(".header__logo_m").removeClass("header__logo");
   }
 
   $burger.on("click", function (event) {
@@ -43,6 +42,14 @@ jQuery(document).ready(function ($) {
   }
 
   $(".guests__slider").slick({
+    slidesToShow: 1,
+    arrows: false,
+    cssEase: "linear",
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+
+  $(".main-news__slider").slick({
     slidesToShow: 1,
     arrows: false,
     cssEase: "linear",
